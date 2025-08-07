644d43e5a11dcf1e7668f7d1a06bf8b45fa00ff6
jesualujan <lujan.oficial@gmail.com>
2025-08-07 17:42:58 -0600

    ✨ feat vite.config: configuración mejorada y plugins añadidos

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // habiliar la opción de react fast refresh para una experiencia de desarrollo más fluida
+    fastRefresh: true,
+  })],
+  server: {
+    port: 3000, // cambiar el puerto si quieres ( por defecto es 5173)
+    open: true, // abrir el navegador automáticamente al iniciar el servidor
+    strictPort: true // fallará si el puerto está ocupado (evita que vite elija otro automaticamente))
+  }
 })
