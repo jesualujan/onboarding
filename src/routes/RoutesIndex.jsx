eea3790da48c6071c7bc2267c490dea468013a7e
jesualujan <lujan.oficial@gmail.com>
2025-08-08 13:32:00 -0600

    ✨ feat Routes: creación de nuestras rutas públicas

---
+++ src/routes/RoutesIndex.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Login from "../pages/Login/Login";
+import Secret from "../pages/Secret/Secret";
+import Signup from "../pages/Signup/Signup";
+
+const RoutesIndex = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/secret" element={<Secret />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
+  );
+};
+
+export default RoutesIndex;
