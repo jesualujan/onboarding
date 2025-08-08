24205c2b33aeb262e12ecc7185dcfe374d93390d
jesualujan <lujan.oficial@gmail.com>
2025-08-08 15:33:27 -0600

    ✨ feat alias: implementación del alias

--- src/routes/RoutesIndex.jsx
+++ src/routes/RoutesIndex.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/Home";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import Login from "../pages/Login/Login";
-import Secret from "../pages/Secret/Secret";
-import Signup from "../pages/Signup/Signup";
+import Home from "@/pages/Home/Home";
+import Dashboard from "@/pages/Dashboard/Dashboard";
+import Login from "@/pages/Login/Login";
+import Secret from "@/pages/Secret/Secret";
+import Signup from "@/pages/Signup/Signup";
 
 const RoutesIndex = () => {
   return (
