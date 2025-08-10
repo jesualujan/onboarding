ff7c205bc58df0ea513d85707d3f7ae413cf7637
jesualujan <lujan.oficial@gmail.com>
2025-08-09 22:32:31 -0600

    ✨ feat app: importamos el contexto en app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import Header from "@/components/Header";
 import RoutesIndex from "@/routes/RoutesIndex";
 import { ToastContainer } from "react-toastify"; // importar el contenedor de notificaciones
 import "react-toastify/dist/ReactToastify.css"; // importar el estilo de las notificaciones de toastify
+import { AuthProvider } from "@/context/AuthContext";
 
 function App() {
   return (
     <>
       {/* EL <ToastContainer /> es el que mostrará las notificaciones en toda mi aplicación, tienes react-toastify configurado globalmente */}
       <ToastContainer />
-      <Header />
-      <RoutesIndex />
+      <AuthProvider>
+        <Header />
+        <RoutesIndex />
+      </AuthProvider>
     </>
   );
 }
